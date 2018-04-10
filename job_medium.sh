24f5dce9fac7f8034d6a0bc6e8a1e5f11a37a773
Greg <ghunkins@users.noreply.github.com>
2018-04-09 23:41:42 -0400

    medium added

---
+++ job_medium.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -t 5-00:00:00
+#SBATCH --job-name=began
+#SBATCH --output=output_beganII_%j.txt
+#SBATCH -e error_beganII_%j.txt
+#SBATCH --gres=gpu:2
+
+source activate BEGAN
+python main.py --batch_size 4 --input_scale_size 64
