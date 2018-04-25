28994d6cde833c5a5b5aa6c704065959cf98a3ec
Greg <ghunkins@users.noreply.github.com>
2018-04-24 20:25:59 -0400

    move file

---
+++ encode_interpolate.py
@@ -0,0 +1,52 @@
+"""
+test.py
+Programmer: Gregory D. Hunkins
+"""
+import os
+import tensorflow as tf
+
+from trainer import Trainer
+from config import get_config
+from data_loader import get_loader
+from utils import prepare_dirs_and_logger
+
+def test(config):
+    prepare_dirs_and_logger(config)
+    tf.set_random_seed(config.random_seed)
+
+    if config.is_train:
+        raise Exception("[!] Training is not supported for this method.")
+
+    size = config.input_scale_size
+
+    if config.test_type == 'encode':
+        dataset = config.test_data_dir or config.dataset             # e.g. 'CelebA'
+        data_loader = get_loader(                                    # get a fake loader
+            dataset, config.batch_size, config.input_scale_size,
+            config.data_format, config.split)
+        trainer = Trainer(config, data_loader)                       # initialize Trainer
+
+        dataset_path = os.path.join(config.data_dir, dataset)       # get path for dataset
+
+        trainer.encode_save(dataset_path, size)                     # call encode save
+
+    elif config.test_type == 'interpolate':
+        dataset1 = config.test_data_dir or config.dataset           # e.g. 'CelebA'
+        dataset2 = config.dataset2                                  # e.g. 'CelebB'
+        data_loader = get_loader(                                   # get a fake loader
+            dataset1, config.batch_size, config.input_scale_size,   
+            config.data_format, config.split)
+        trainer = Trainer(config, data_loader)                      # initialize Trainer
+
+        dataset1_path = os.path.join(config.data_dir, dataset1)     # get path for dataset 1
+        dataset2_path = os.path.join(config.data_dir, dataset2)     # get path for dataset 2
+
+        trainer.interpolate_encode_save(dataset1_path, dataset2_path, size)           # call encode interpolate save
+        
+    else:
+        raise Exception("[!] Test type {} is not supported for this method.".format(config.test_type))
+
+
+if __name__ == "__main__":
+    config, unparsed = get_config()
+    test(config)
