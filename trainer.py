e258f2512902ab8bba7103b63b5f61557dfa4184
Greg <ghunkins@users.noreply.github.com>
2018-05-13 01:00:20 -0400

    try outside

--- trainer.py
+++ trainer.py
@@ -316,9 +316,9 @@ class Trainer(object):
 
             print('Mom x:', self.mom_x.shape)
             print('Dad x:', self.dad_x.shape)
-            # encoding
-            dad_encode = self.encode(self.dad_x)
-            mom_encode = self.encode(self.mom_x)
+
+            _, dad_encode = np.split(self.encode(self.dad_x))
+            _, mom_encode = np.split(self.encode(self.mom_x))
             print('Mom encode:', mom_encode.shape)
             print('Dad encode:', dad_encode.shape)
             #encode = slerp(0.5, dad_encode, mom_encode)
